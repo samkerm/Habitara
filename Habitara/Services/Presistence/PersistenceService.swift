fb0207cec8f953ca2519866e7d850b7bbcd90b85
Sam Kheirandish <samkheirandish@gmail.com>
2022-09-18 20:50:01 -0700

    Added dashboard UI tests

--- Habitara/Services/Presistence/PersistenceService.swift
+++ Habitara/Services/Presistence/PersistenceService.swift
@@ -49,6 +49,11 @@ extension Services {
             if inMemory {
                 container.persistentStoreDescriptions.first!.url = URL(fileURLWithPath: "/dev/null")
             }
+            
+            #if TESTS
+                container.persistentStoreDescriptions.first!.url = URL(fileURLWithPath: "/dev/null")
+            #endif
+            
             container.loadPersistentStores(completionHandler: { (storeDescription, error) in
                 if let error = error as NSError? {
                     // Replace this implementation with code to handle the error appropriately.
@@ -65,6 +70,11 @@ extension Services {
                     fatalError("Unresolved error \(error), \(error.userInfo)")
                 }
             })
+            
+            #if TESTS
+                addSomeItems()
+            #endif
+            
             container.viewContext.automaticallyMergesChangesFromParent = true
             assignPublisherForItems()
         }
@@ -105,6 +115,21 @@ extension Services {
                     self?.items.send($0)
                 }
                 .store(in: &cancelables)
+        }
+        
+        private func addSomeItems() {
+            for _ in 0..<10 {
+                let newItem = Item(context: container.viewContext)
+                newItem.timestamp = Date()
+            }
+            do {
+                try container.viewContext.save()
+            } catch {
+                // Replace this implementation with code to handle the error appropriately.
+                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+                let nsError = error as NSError
+                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
+            }
         }
         
         // MARK: Internal
